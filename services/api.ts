12844b20da3347ef8cd4643d2880705aecb90823
Jeret Goad <jeretgoad@Mac.TwinLakes>
2025-05-14 01:45:20 -0500

    Added styles, home screen UI,routing/ navigation tabs, and began backend api calls

---
+++ services/api.ts
@@ -0,0 +1,13 @@
+{/* const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTlmNWU3M2Y2ZDM2Yjg3YzFkMjJiNTJhYzY4ZTc1YSIsIm5iZiI6MTc0NzIwMzEzOC4zNjQsInN1YiI6IjY4MjQzNDQyYmY4N2ZjNDM3MDJkMjUyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lD4ZMq3UnodqfdB7ylaOwIhW3RRrgTDZ_MjIbI7DhDg'
+    }
+};
+
+fetch(url, options)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error(err)); */}
