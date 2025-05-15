be77ab48034a3a49715cb2ab9e1c663dd300cdb6
Jeret Goad <jeretgoad@Mac.TwinLakes>
2025-05-14 20:30:59 -0500

    Finished the home screen UI, added API calls to TMDB, and added the display and data fetch for the movie cards

--- services/api.ts
+++ services/api.ts
@@ -1,13 +1,26 @@
-{/* const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-const options = {
-    method: 'GET',
+export const TMDB_CONFIG = {
+    BASE_URL: 'https:api.themoviedb.org/3',
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTlmNWU3M2Y2ZDM2Yjg3YzFkMjJiNTJhYzY4ZTc1YSIsIm5iZiI6MTc0NzIwMzEzOC4zNjQsInN1YiI6IjY4MjQzNDQyYmY4N2ZjNDM3MDJkMjUyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lD4ZMq3UnodqfdB7ylaOwIhW3RRrgTDZ_MjIbI7DhDg'
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
     }
-};
+}
 
-fetch(url, options)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.error(err)); */}
+export const fetchMovies = async  ({ query }: { query: string}) => {
+    const endpoint = query
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, { method: 'GET', headers: TMDB_CONFIG.headers });
+
+    if(!response.ok) {
+        // @ts-ignore
+        throw new Error('Something went wrong', response.statusText );
+    }
+
+    const data = await response.json();
+
+    return data.results;
+}
+
